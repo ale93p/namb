21f186cbb83e04e6b577cc7f19de5ddb94cdf197
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-01-23 15:26:14 +0100

    * generalized boxplot generation (close #23, close #24, close #25) * choice between single test or all

--- tests/logs/generate_plots.py
+++ tests/logs/generate_plots.py
@@ -2,25 +2,14 @@
 
 import sys 
 import os
+import argparse
 import plotting as p
 
 plots_dir = './plots'
 
-def main(test_name):
-    if test_name == "common_individual":
-        p.common_individual(out = plots_dir)
-    else:
-        print("Sorry, no plotting has been implemented for this results yet.")
-
-def print_help(err_code, opts = {}):
-    if err_code == 1:
-        print("Please chose the test you want to generate the plots for")
-    if err_code == 2:
-        print("Please chose a valid test name between the available folders")
-        print("Available tests:")
-        for name in opts["tests_names"]: print("  * " + name)
-    print("\nUsage:")
-    print("  " + sys.argv[0] + " test_name")    
+def main(test_name, args):
+    if args.just_show: p.boxplot(test_name)
+    else: p.boxplot(test_name, out = plots_dir)
 
 def init():
     if not os.path.exists(plots_dir):
@@ -32,12 +21,19 @@ if __name__ == "__main__":
     valid_names = [directory[0].replace('./','') for directory in os.walk('.') \
         if directory[0] != '.' and directory[0] != plots_dir and 'pycache' not in directory[0]]
     
-    init()
-
-    if len(sys.argv) < 2: err = 1
-    elif sys.argv[1] not in valid_names: 
-        err = 2
-        opts = {"tests_names" : valid_names}
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="Generate plots for tests results")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("test_name", metavar="test_name", type=str, nargs='?', choices=valid_names, help="generates plots for the test name specified")
+    group.add_argument("-a", "--all", dest="all_tests", action="store_true", default=False, help="generates plots for all the available tests")
+    parser.add_argument("-s", "--show", dest="just_show", action="store_true", default=False, help="only shows the plot instead of saving the pdf")
     
-    if err != 0: print_help(err, opts)
-    else: main(sys.argv[1])
+
+    args = parser.parse_args()
+    init()
+    
+    if args.all_tests:
+        for test in valid_names:
+            main(test, args)
+    
+    else:
+        main(args.test_name[0], args)
--- tests/logs/plotting.py
+++ tests/logs/plotting.py
@@ -4,11 +4,12 @@ import re
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def common_individual(out = None):
-    root = "./common_individual/"
+def boxplot(root, out = None):
+
     cpu_to_plot = {}
-    for csv in [f for f in glob.glob(root + "*.csv")]:
-        task_name = re.search('_([a-z]+?)_bolt', csv).group(1)
+    re_pattern = '([0-9]*)_([a-z]+?)_(bolt|[0-9]*_)'
+    for csv in [f for f in sorted(glob.glob(os.path.join(root, "*.csv")), key=lambda k: re.search(re_pattern, k).group(2))]:
+        task_name = re.search(re_pattern, csv).group(2)
         df = pd.read_csv(csv, sep=',')
         cpu_time = df["tot"] \
             .diff() \
@@ -16,11 +17,12 @@ def common_individual(out = None):
             .iloc[1:] \
             .tolist() 
         cpu_to_plot[task_name] = cpu_time
-        
+    
+
     labels, data = cpu_to_plot.keys(), cpu_to_plot.values()
     plt.boxplot(data)
     plt.xticks(range(1, len(labels) + 1), labels)
     plt.ylabel("CPU Load (ms)")
-    plt.title("Individual tasks benchmark")
-    if(out): plt.savefig(os.path.join(out, "common_individual_" + task_name + ".pdf"))
+    plt.title(root.replace("_", " "))
+    if(out): plt.savefig(os.path.join(out, "{}_boxplot.pdf".format(root)))
     else: plt.show()
