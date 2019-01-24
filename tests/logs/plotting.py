088eb5308ee850390273f7814b1b254847c19246
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-01-24 15:03:24 +0100

    fixed pattern and plotting

--- tests/logs/plotting.py
+++ tests/logs/plotting.py
@@ -5,15 +5,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def boxplot(root, opts = {}):
-    bw = 'busywait' in root
     cpu_to_plot = {}
-    re_pattern = '([0-9]*)_([a-z]+?|[a-z]+_[0-9]+?)_bolt_'
-    if bw:
-        sorted_files = [f for f in sorted(glob.glob(os.path.join(root, "*.csv")), 
-                                    key=lambda k: int( re.search(re_pattern, k).group(2).replace("busywait_","") ))]
-    else:
-        sorted_files = [f for f in sorted(glob.glob(os.path.join(root, "*.csv")), 
-                                    key=lambda k: re.search(re_pattern, k).group(2))]
+    re_pattern = '([0-9]*)_([a-z]+_?[a-z0-9]+)_(bolt|bw)_'
+    sorted_files = [f for f in sorted(glob.glob(os.path.join(root, "*.csv")), 
+                                key=lambda k: re.search(re_pattern, k).group(2))]
     
     for csv in sorted_files:
         
@@ -28,11 +23,12 @@ def boxplot(root, opts = {}):
     
 
     labels, data = cpu_to_plot.keys(), cpu_to_plot.values()
-    if bw: labels = [l.replace("busywait_", "") for l in labels]
+    # labels = [l.replace("busywait_", "") for l in labels]
     plt.boxplot(data)
     plt.xticks(range(1, len(labels) + 1), labels)
     plt.ylabel("CPU Load (ms)")
-    if bw: plt.xlabel("x1000 cycles")
+    plt.ylim(0,300)
+    # plt.xlabel("x1000 cycles")
     plt.title(root.replace("_", " "))
 
     output_name = "{}_boxplot.{}".format(root, opts["img_format"])
