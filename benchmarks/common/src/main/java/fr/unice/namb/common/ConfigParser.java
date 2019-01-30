62956db74bd5050af55487843e7f57086524613b
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-01-30 11:40:01 +0100

    Added configuration parser

--- benchmarks/common/src/main/java/fr/unice/namb/common/ConfigParser.java
+++ benchmarks/common/src/main/java/fr/unice/namb/common/ConfigParser.java
@@ -1,4 +1,23 @@
 package fr.unice.namb.common;
 
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
 public class ConfigParser {
+
+    public static ConfigScheme parseConfigurationFile(File confFile){
+
+        ConfigScheme conf = null;
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        try{
+            conf = mapper.readValue(confFile, ConfigScheme.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conf;
+
+    }
+
 }
