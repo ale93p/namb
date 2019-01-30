edb7d775986bbdf3151ccfbcae1d7cef618cb11f
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-01-30 17:19:34 +0100

    Added TextSpout (#33)

--- benchmarks/storm/src/main/java/fr/unice/namb/storm/spouts/TextSpout.java
+++ benchmarks/storm/src/main/java/fr/unice/namb/storm/spouts/TextSpout.java
@@ -1,5 +1,6 @@
 package fr.unice.namb.storm.spouts;
 
+import fr.unice.namb.common.ConfigDefaults.Distribution;
 import org.apache.storm.spout.SpoutOutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
@@ -8,42 +9,59 @@ import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
 import org.apache.storm.utils.Utils;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Map;
 
 public class TextSpout extends BaseRichSpout {
 
     private SpoutOutputCollector _collector;
-    private long count;
+
+    private int dataSize;
     private long sleepTime;
-    private String sampleText = "text samle";
+    private Distribution distribution;
+    private String payload;
+    private long count;
 
-    public TextSpout(){
-        this.sleepTime = 1000;
+    public TextSpout(int dataSize, Distribution distribution, int rate) {
+        this.dataSize = dataSize;
+        this.sleepTime = convertToInterval(rate);
+        this.distribution = distribution;
     }
 
-    public TextSpout(long millis){
-        this.sleepTime = millis;
+    private long convertToInterval(int msgPerSec){
+        return 1000/msgPerSec; // Interval in ms
     }
 
-    @Override
+    private String generatePayload(int size){
+        byte[] payload = new byte[size];
+        Arrays.fill(payload, (byte) 'P');
+        return new String(payload, StandardCharsets.UTF_8);
+    }
+
     public void open(Map conf, TopologyContext context, SpoutOutputCollector collector){
+        this.payload = generatePayload(this.dataSize);
         this.count = 0;
         this._collector = collector;
     }
 
-    @Override
-    public void activate(){ }
-
     public void nextTuple(){
-        Utils.sleep(this.sleepTime);
-        _collector.emit(new Values(this.sampleText), count++);
-        System.out.println("emitted: \"" + this.sampleText + "\"");
+        switch(this.distribution){
+            case uniform:
+                Utils.sleep(this.sleepTime);
+                _collector.emit(new Values(this.payload), count++);
+                break;
+            case burst:
+                //TODO: find a way to generate bursts from time to time
+                //Utils.sleep(this.sleepTime);
+                //_collector.emit(new Values(this.payload), count++);
+                break;
+        }
+
     }
 
-    @Override
     public void ack(Object msgId){ super.ack(msgId); }
 
-    @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
         declarer.declare(new Fields("text"));
     }
