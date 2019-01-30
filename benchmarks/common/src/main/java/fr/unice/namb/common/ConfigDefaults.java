7ff3aaab05f2e9495d1f2f773f4b7fde46d5eac3
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-01-30 11:50:24 +0100

    Merging Configuration Parser (#40)
    * added common module
    
    * Added dependencies for jackson
    
    * Added configuration default values
    
    * Added class scheme for jackson parsing
    
    * Added configuration parser
    
    * Added module common to project
    
    * Updated parameters naming
    
    * removed useless folder

---
+++ benchmarks/common/src/main/java/fr/unice/namb/common/ConfigDefaults.java
@@ -0,0 +1,35 @@
+package fr.unice.namb.common;
+
+public class ConfigDefaults {
+
+    public enum ConnectionShape{
+        linear, diamond, star
+    }
+
+
+    public enum Balancing{
+        balanced, increasing, decresing, bell
+    }
+
+    public enum DataType{
+        numeric, text
+    }
+
+    public enum Distribution{
+        uniform, normal //TODO: add burst, saw-tooth, bimodal
+    }
+
+    public static final int TOTAL_TEST_DURATION = 300;
+    public static final int DF_DEPTH = 3;
+    public static final int DF_SCALABILITY_PARALLELISM = 10;
+    public static final ConnectionShape DF_CONNECTION_SHAPE = ConnectionShape.linear;
+    public static final boolean DF_TRAFFIC_BALANCING = true;
+    public static final boolean DF_MESSAGE_RELIABILITY = true;
+    public static final int DF_WORKLOAD_PROCESSING = 300;
+    public static final Balancing DF_WORKLOAD_BALANCING = Balancing.balanced;
+    public static final int DS_SYNTHETIC_DATA_SIZE = 8;
+    public static final DataType DS_SYNTHETIC_DATA_TYPE = DataType.text;
+    public static final Distribution DS_SYNTHETIC_ARRIVAL_DISTRIBUTION = Distribution.uniform;
+
+
+}
