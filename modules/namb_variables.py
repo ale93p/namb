040ad24110ab5530b93812c5f887f52a03a611b3
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-02-04 18:44:22 +0100

    Updated version methodology

--- modules/namb_variables.py
+++ modules/namb_variables.py
@@ -1,6 +1,8 @@
 import os
 
+STORM_BENCHMARK_VERSION='0.1.0-alpha'
+
 CWD = os.getcwd()
-CONF_PATH = CWD + "/conf"
-STORM_JAR = CWD + "/benchmarks/storm/target/storm-1.0-SNAPSHOT.jar"
+CONF_PATH = "{}/conf".format(CWD)
+STORM_JAR = "{}/benchmarks/storm/target/storm-{}.jar".format(CWD, STORM_BENCHMARK_VERSION)
 STORM_CLASS = "fr.unice.namb.storm.BenchmarkApplication"
