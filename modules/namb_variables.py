0779a51309ae6416dcdf74f9de46041feec285f3
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-02-05 10:28:27 +0100

    Fixed main project path

--- modules/namb_variables.py
+++ modules/namb_variables.py
@@ -2,7 +2,7 @@ import os
 
 STORM_BENCHMARK_VERSION='0.1.0-alpha'
 
-CWD = os.getcwd()
+CWD = os.path.dirname(os.path.realpath(__file__)).replace('/modules','')
 CONF_PATH = "{}/conf".format(CWD)
 STORM_JAR = "{}/benchmarks/storm/target/storm-{}.jar".format(CWD, STORM_BENCHMARK_VERSION)
 STORM_CLASS = "fr.unice.namb.storm.BenchmarkApplication"
