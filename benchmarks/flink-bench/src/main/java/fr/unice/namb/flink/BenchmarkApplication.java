a21711b92f0b17d7684cb46023e7295acfff75c7
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-02-27 17:32:37 +0100

    Added basic application generation: depth, workload, parallelism

--- benchmarks/flink-bench/src/main/java/fr/unice/namb/flink/BenchmarkApplication.java
+++ benchmarks/flink-bench/src/main/java/fr/unice/namb/flink/BenchmarkApplication.java
@@ -1,14 +1,18 @@
 package fr.unice.namb.flink;
 
 import fr.unice.namb.flink.connectors.SyntheticConnector;
+import fr.unice.namb.flink.operators.BusyWaitMap;
 import fr.unice.namb.utils.common.AppBuilder;
 import fr.unice.namb.utils.configuration.Config;
 import fr.unice.namb.utils.configuration.schema.NambConfigSchema;
+import org.apache.commons.lang3.tuple.MutablePair;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Map;
 
 public class BenchmarkApplication {
 
@@ -38,18 +42,46 @@ public class BenchmarkApplication {
         int                     rate                = conf.getDatastream().getSynthetic().getFlow().getRate();
 
         // Bolt-specific configurations
-        int     numberOfBolts   = app.getTotalComponents() - numberOfSources;
-        boolean reliability     = conf.getDataflow().isReliable();
+        int     numberOfOperators   = app.getTotalComponents() - numberOfSources;
+        boolean reliability         = conf.getDataflow().isReliable();
 
         Iterator<Integer> cpIterator    = componentsParallelism.iterator();
-        ArrayList<String> sourcesList    = new ArrayList<>();
-        ArrayList<String> boltsList     = new ArrayList<>();
+        ArrayList<MutablePair<String, SingleOutputStreamOperator>> sourcesList = new ArrayList<>();
+        ArrayList<MutablePair<String, SingleOutputStreamOperator>> operatorsList = new ArrayList<>();
 
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         String sourceName = "source";
-        DataStream<String> source = env.addSource(new SyntheticConnector(dataSize, dataValues, dataValuesBalancing, distribution, rate), sourceName).setParallelism(cpIterator.next());
+        DataStream<String> source = env.addSource(new SyntheticConnector(dataSize, dataValues, dataValuesBalancing, distribution, rate))
+                                        .setParallelism(cpIterator.next())
+                                        .name("source1");
+
+        int operatorID = 1;
+        int cycles;
+        String operatorName;
+
+        for(int i = 1; i<depth; i++){
+            SingleOutputStreamOperator op = null;
+            operatorName = "op_" + operatorID;
+            cycles = app.getNextProcessing();
+            if(i==1) {
+                 op = source
+                         .map(new BusyWaitMap(cycles))
+                         .setParallelism(cpIterator.next())
+                         .name(operatorName);
+            }
+            else{
+                 op = operatorsList.get(i-2).getRight()
+                         .map(new BusyWaitMap(cycles))
+                         .setParallelism(cpIterator.next())
+                         .name(operatorName);
+
+            }
+            operatorsList.add(new MutablePair<String, SingleOutputStreamOperator>(operatorName, op));
+            operatorID++;
+        }
+
 
         return env;
 
