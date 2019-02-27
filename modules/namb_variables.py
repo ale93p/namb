c37c97e2a4c817e9af2aed4b1a65c46b98691d4b
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-02-27 17:31:42 +0100

    Fixed jar naming

--- modules/namb_variables.py
+++ modules/namb_variables.py
@@ -2,7 +2,7 @@ import os
 import xml.etree.ElementTree as xml_parser
 
 CWD = os.path.dirname(os.path.realpath(__file__)).replace('/modules','')
-NAMB_POM = "{}/benchmarks/pom.xml".format(CWD)
+NAMB_POM = "{}/pom.xml".format(CWD)
 
 pom = xml_parser.parse(NAMB_POM)
 
@@ -15,9 +15,9 @@ CONF_PATH = "{}/conf".format(CWD)
 NAMB_CONF = "{}/namb.yml".format(CONF_PATH)
 
 STORM_CONF = "{}/storm-benchmark.yml".format(CONF_PATH)
-STORM_JAR = "{}/benchmarks/storm-bench/target/storm-{}.jar".format(CWD, STORM_BENCHMARK_VERSION)
+STORM_JAR = "{}/benchmarks/storm-bench/target/storm-bench-{}.jar".format(CWD, STORM_BENCHMARK_VERSION)
 STORM_CLASS = "fr.unice.namb.storm.BenchmarkApplication"
 
 HERON_CONF = "{}/heron-benchmark.yml".format(CONF_PATH)
-HERON_JAR = "{}/benchmarks/heron-bench/target/heron-{}.jar".format(CWD, HERON_BENCHMARK_VERSION)
+HERON_JAR = "{}/benchmarks/heron-bench/target/heron-bench-{}.jar".format(CWD, HERON_BENCHMARK_VERSION)
 HERON_CLASS = "fr.unice.namb.heron.BenchmarkApplication"
