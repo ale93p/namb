09e884a2267a6fdc61439c572a26e1ec2ab32383
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-03-01 17:40:09 +0100

    Added topology shape generation

--- benchmarks/flink-bench/src/main/java/fr/unice/namb/flink/BenchmarkApplication.java
+++ benchmarks/flink-bench/src/main/java/fr/unice/namb/flink/BenchmarkApplication.java
@@ -65,8 +65,8 @@ public class BenchmarkApplication {
         if (numberOfSources > 1){
             sourceName = "unified_source";
             DataStream<String> unifiedSource = sourcesList.get(0).getRight().union(sourcesList.get(1).getRight());
-            for(int i=2; i<numberOfSources; i++){
-                unifiedSource.union(sourcesList.get(i).getRight());
+            for(int s=2; s<numberOfSources; s++){
+                unifiedSource.union(sourcesList.get(s).getRight());
             }
             sourcesList.add(new MutablePair<>(sourceName, unifiedSource));
         }
@@ -77,28 +77,52 @@ public class BenchmarkApplication {
         String operatorName;
 
         for(int i = 1; i<depth; i++){
+            int levelWidth = dagLevelsWidth.get(i);
             SingleOutputStreamOperator<String> op = null;
-            operatorName = "op_" + operatorID;
-            cycles = app.getNextProcessing();
             if(i==1) {
-                op = sourcesList.get(sourcesList.size() - 1).getRight()
-                        .map(new BusyWaitMap(cycles))
-                        .setParallelism(cpIterator.next())
-                        .name(operatorName);
+                for(int opCount=0; opCount<levelWidth; opCount++) {
+                    operatorName = "op_" + operatorID;
+                    cycles = app.getNextProcessing();
+                    op = sourcesList.get(sourcesList.size() - 1).getRight()
+                            .map(new BusyWaitMap(cycles))
+                            .setParallelism(cpIterator.next())
+                            .name(operatorName);
+                    operatorsList.add(new MutablePair<>(operatorName, op));
+                    operatorID++;
+                }
             }
             else{
-                SingleOutputStreamOperator<String> parent = operatorsList.get(i-2).getRight();
-                op = parent
-                        .map(new BusyWaitMap(cycles))
-                        .setParallelism(cpIterator.next())
-                        .name(operatorName);
-
+                if(topologyShape == Config.ConnectionShape.diamond && dagLevelsWidth.get(i-1) > 1){ // diamond shape union
+                    DataStream<String> diamondUnion = operatorsList.get(operatorID - 2).getRight().union(operatorsList.get(operatorID - 3).getRight());
+                    for(int o=2; o<dagLevelsWidth.get(i-1); o++){
+                        diamondUnion.union(operatorsList.get(o).getRight());
+                    }
+                    operatorName = "op_" + operatorID;
+                    cycles = app.getNextProcessing();
+                    op = diamondUnion
+                            .map(new BusyWaitMap(cycles))
+                            .setParallelism(cpIterator.next())
+                            .name(operatorName);
+                    operatorsList.add(new MutablePair<>(operatorName, op));
+                    operatorID++;
+                }
+                else{
+                    int parentOperatorIdx = (topologyShape == Config.ConnectionShape.diamond ||
+                            (topologyShape == Config.ConnectionShape.star && i>3)) ? i - 1 : i - 2;
+                    SingleOutputStreamOperator<String> parent = operatorsList.get(parentOperatorIdx).getRight();
+                    for(int opCount = 0; opCount<levelWidth; opCount++){
+                        operatorName = "op_" + operatorID;
+                        cycles = app.getNextProcessing();
+                        op = parent
+                                .map(new BusyWaitMap(cycles))
+                                .setParallelism(cpIterator.next())
+                                .name(operatorName);
+                        operatorsList.add(new MutablePair<>(operatorName, op));
+                        operatorID++;
+                    }
+                }
             }
-            operatorsList.add(new MutablePair<>(operatorName, op));
-            operatorID++;
         }
-
-
         return env;
 
     }
