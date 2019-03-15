ebdb431a1fa96111d58dceda776b000c16795627
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-03-15 16:52:48 +0100

    [Heron] Added Windowed bolt

---
+++ benchmarks/heron-bench/src/main/java/fr/unice/yamb/heron/bolts/WindowedBusyWaitBolt.java
@@ -0,0 +1,46 @@
+package fr.unice.yamb.heron.bolts;
+
+import com.twitter.heron.api.bolt.BaseWindowedBolt;
+import com.twitter.heron.api.bolt.OutputCollector;
+import com.twitter.heron.api.topology.OutputFieldsDeclarer;
+import com.twitter.heron.api.topology.TopologyContext;
+import com.twitter.heron.api.tuple.Fields;
+import com.twitter.heron.api.tuple.Tuple;
+import com.twitter.heron.api.tuple.Values;
+import com.twitter.heron.api.windowing.TupleWindow;
+
+import java.util.Map;
+
+public class WindowedBusyWaitBolt extends BaseWindowedBolt {
+    private OutputCollector _collector;
+    private long _cycles;
+
+
+    public WindowedBusyWaitBolt(long cycles){
+        this._cycles = cycles;
+    }
+
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector){
+        this._collector = collector;
+    }
+
+    public void execute(TupleWindow inputWindow){
+
+        Object payload = null;
+
+        for (Tuple tuple : inputWindow.get()) {
+            payload = tuple.getValue(0);
+            // simulate processing load
+            for (long i = 0; i < _cycles; i++) {
+            }
+        }
+
+
+        _collector.emit(new Values(payload));
+
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer){ declarer.declare(new Fields("value"));}
+
+
+}
