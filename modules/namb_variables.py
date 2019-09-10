b0d70a7f6d58c757df377cab7efd767b2207932f
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-09-10 16:59:16 +0200

    fix variables naming issue

--- modules/namb_variables.py
+++ modules/namb_variables.py
@@ -2,11 +2,11 @@ import os
 import xml.etree.ElementTree as xml_parser
 
 CWD = os.path.dirname(os.path.realpath(__file__)).replace('/modules','')
-YAMB_POM = "{}/pom.xml".format(CWD)
+NAMB_POM = "{}/pom.xml".format(CWD)
 
-pom = xml_parser.parse(YAMB_POM)
+pom = xml_parser.parse(NAMB_POM)
 
-YAMB_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}revision").text
+NAMB_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}revision").text
 
 STORM_BENCHMARK_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}storm.benchmark.version").text
 HERON_BENCHMARK_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}heron.benchmark.version").text
@@ -15,15 +15,15 @@ FLINK_BENCHMARK_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0
 CONF_PATH = "{}/conf".format(CWD)
 CONF_PATH_DEFAULTS = "{}/defaults".format(CONF_PATH)
 
-YAMB_CONF = "{}/yamb.yml".format(CONF_PATH)
+NAMB_CONF = "{}/namb.yml".format(CONF_PATH)
 
 STORM_CONF = "{}/storm-benchmark.yml".format(CONF_PATH)
 STORM_JAR = "{}/benchmarks/storm-bench/target/storm-bench-{}.jar".format(CWD, STORM_BENCHMARK_VERSION)
-STORM_CLASS = "fr.unice.yamb.storm.BenchmarkApplication"
+STORM_CLASS = "fr.unice.namb.storm.BenchmarkApplication"
 
 HERON_CONF = "{}/heron-benchmark.yml".format(CONF_PATH)
 HERON_JAR = "{}/benchmarks/heron-bench/target/heron-bench-{}.jar".format(CWD, HERON_BENCHMARK_VERSION)
-HERON_CLASS = "fr.unice.yamb.heron.BenchmarkApplication"
+HERON_CLASS = "fr.unice.namb.heron.BenchmarkApplication"
 
 FLINK_CONF = "{}/flink-benchmark.yml".format(CONF_PATH)
 FLINK_JAR = "{}/benchmarks/flink-bench/target/flink-bench-{}.jar".format(CWD, FLINK_BENCHMARK_VERSION)
