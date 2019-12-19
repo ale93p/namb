89282c8dc08c44b63e9115a5619a8c36db7b92c3
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-12-19 13:51:52 +0100

    Fixed path creation for Windows system support

--- modules/namb_variables.py
+++ modules/namb_variables.py
@@ -1,25 +1,25 @@
 import os
+from os.path import join
 import xml.etree.ElementTree as xml_parser
 
-CWD = os.path.dirname(os.path.realpath(__file__)).replace('/modules','')
-NAMB_POM = "{}/pom.xml".format(CWD)
-
+CWD = os.path.dirname(os.path.realpath(__file__)).replace('/modules','').replace('\modules','')
+NAMB_POM = join(CWD,"pom.xml")
 pom = xml_parser.parse(NAMB_POM)
 
 NAMB_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}revision").text
 
-CONF_PATH = "{}/conf".format(CWD)
-CONF_PATH_DEFAULTS = "{}/defaults".format(CONF_PATH)
+CONF_PATH = join(CWD,"conf")
+CONF_PATH_DEFAULTS = join(CONF_PATH,"defaults")
 
-NAMB_CONF = "{}/namb.yml".format(CONF_PATH)
+NAMB_CONF = join(CONF_PATH,"namb.yml")
 
-STORM_CONF = "{}/storm-benchmark.yml".format(CONF_PATH)
-STORM_JAR = "{}/benchmarks/storm-bench/target/storm-bench-{}.jar".format(CWD, NAMB_VERSION)
+STORM_CONF = join(CONF_PATH,"storm-benchmark.yml")
+STORM_JAR = join(CWD,"benchmarks","storm-bench","target","storm-bench-{}.jar".format(NAMB_VERSION))
 STORM_CLASS = "fr.unice.namb.storm.BenchmarkApplication"
 
-HERON_CONF = "{}/heron-benchmark.yml".format(CONF_PATH)
-HERON_JAR = "{}/benchmarks/heron-bench/target/heron-bench-{}.jar".format(CWD, NAMB_VERSION)
+HERON_CONF = join(CONF_PATH,"heron-benchmark.yml")
+HERON_JAR = join(CWD,"benchmarks","heron-bench","target","heron-bench-{}.jar".format(NAMB_VERSION))
 HERON_CLASS = "fr.unice.namb.heron.BenchmarkApplication"
 
-FLINK_CONF = "{}/flink-benchmark.yml".format(CONF_PATH)
-FLINK_JAR = "{}/benchmarks/flink-bench/target/flink-bench-{}.jar".format(CWD, NAMB_VERSION)
+FLINK_CONF = join(CONF_PATH,"flink-benchmark.yml")
+FLINK_JAR = join(CWD,"benchmarks","flink-bench","target","flink-bench-{}.jar".format(NAMB_VERSION))
