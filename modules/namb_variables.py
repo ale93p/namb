e26ca60f19c94987939e25b1de83689ea96fdb53
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-12-19 11:27:08 +0100

    Fixed wrong application version

--- modules/namb_variables.py
+++ modules/namb_variables.py
@@ -8,22 +8,18 @@ pom = xml_parser.parse(NAMB_POM)
 
 NAMB_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}revision").text
 
-STORM_BENCHMARK_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}storm.benchmark.version").text
-HERON_BENCHMARK_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}heron.benchmark.version").text
-FLINK_BENCHMARK_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}flink.benchmark.version").text
-
 CONF_PATH = "{}/conf".format(CWD)
 CONF_PATH_DEFAULTS = "{}/defaults".format(CONF_PATH)
 
 NAMB_CONF = "{}/namb.yml".format(CONF_PATH)
 
 STORM_CONF = "{}/storm-benchmark.yml".format(CONF_PATH)
-STORM_JAR = "{}/benchmarks/storm-bench/target/storm-bench-{}.jar".format(CWD, STORM_BENCHMARK_VERSION)
+STORM_JAR = "{}/benchmarks/storm-bench/target/storm-bench-{}.jar".format(CWD, NAMB_VERSION)
 STORM_CLASS = "fr.unice.namb.storm.BenchmarkApplication"
 
 HERON_CONF = "{}/heron-benchmark.yml".format(CONF_PATH)
-HERON_JAR = "{}/benchmarks/heron-bench/target/heron-bench-{}.jar".format(CWD, HERON_BENCHMARK_VERSION)
+HERON_JAR = "{}/benchmarks/heron-bench/target/heron-bench-{}.jar".format(CWD, NAMB_VERSION)
 HERON_CLASS = "fr.unice.namb.heron.BenchmarkApplication"
 
 FLINK_CONF = "{}/flink-benchmark.yml".format(CONF_PATH)
-FLINK_JAR = "{}/benchmarks/flink-bench/target/flink-bench-{}.jar".format(CWD, FLINK_BENCHMARK_VERSION)
+FLINK_JAR = "{}/benchmarks/flink-bench/target/flink-bench-{}.jar".format(CWD, NAMB_VERSION)
