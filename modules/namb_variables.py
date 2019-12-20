c849a490772860181becef7399dbfb2663f42799
Alessio Pagliari <alessio.pagliari@gmail.com>
2019-12-20 11:01:11 +0100

    [v0.7.1] Bug Fixes (#132)
    * Fixed wrong application version
    
    * Commented out kafka section
    
    * Added new test configurations examples
    
    * [v0.7.1] Bump version
    
    * Fixed path creation for Windows system support
    
    * fixed config link

--- modules/namb_variables.py
+++ modules/namb_variables.py
@@ -1,29 +1,25 @@
 import os
+from os.path import join
 import xml.etree.ElementTree as xml_parser
 
-CWD = os.path.dirname(os.path.realpath(__file__)).replace('/modules','')
-NAMB_POM = "{}/pom.xml".format(CWD)
-
+CWD = os.path.dirname(os.path.realpath(__file__)).replace('/modules','').replace('\modules','')
+NAMB_POM = join(CWD,"pom.xml")
 pom = xml_parser.parse(NAMB_POM)
 
 NAMB_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}revision").text
 
-STORM_BENCHMARK_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}storm.benchmark.version").text
-HERON_BENCHMARK_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}heron.benchmark.version").text
-FLINK_BENCHMARK_VERSION = pom.getroot().find("{http://maven.apache.org/POM/4.0.0}properties").find("{http://maven.apache.org/POM/4.0.0}flink.benchmark.version").text
+CONF_PATH = join(CWD,"conf")
+CONF_PATH_DEFAULTS = join(CONF_PATH,"defaults")
 
-CONF_PATH = "{}/conf".format(CWD)
-CONF_PATH_DEFAULTS = "{}/defaults".format(CONF_PATH)
+NAMB_CONF = join(CONF_PATH,"namb.yml")
 
-NAMB_CONF = "{}/namb.yml".format(CONF_PATH)
-
-STORM_CONF = "{}/storm-benchmark.yml".format(CONF_PATH)
-STORM_JAR = "{}/benchmarks/storm-bench/target/storm-bench-{}.jar".format(CWD, STORM_BENCHMARK_VERSION)
+STORM_CONF = join(CONF_PATH,"storm-benchmark.yml")
+STORM_JAR = join(CWD,"benchmarks","storm-bench","target","storm-bench-{}.jar".format(NAMB_VERSION))
 STORM_CLASS = "fr.unice.namb.storm.BenchmarkApplication"
 
-HERON_CONF = "{}/heron-benchmark.yml".format(CONF_PATH)
-HERON_JAR = "{}/benchmarks/heron-bench/target/heron-bench-{}.jar".format(CWD, HERON_BENCHMARK_VERSION)
+HERON_CONF = join(CONF_PATH,"heron-benchmark.yml")
+HERON_JAR = join(CWD,"benchmarks","heron-bench","target","heron-bench-{}.jar".format(NAMB_VERSION))
 HERON_CLASS = "fr.unice.namb.heron.BenchmarkApplication"
 
-FLINK_CONF = "{}/flink-benchmark.yml".format(CONF_PATH)
-FLINK_JAR = "{}/benchmarks/flink-bench/target/flink-bench-{}.jar".format(CWD, FLINK_BENCHMARK_VERSION)
+FLINK_CONF = join(CONF_PATH,"flink-benchmark.yml")
+FLINK_JAR = join(CWD,"benchmarks","flink-bench","target","flink-bench-{}.jar".format(NAMB_VERSION))
